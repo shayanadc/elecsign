706a6bddfad2af66e860084f9675bfe65801de86
Shayan Hosseini <shho@boozt.com>
2024-12-16 22:27:57 +0100

    add transformer test

--- internal/transformer_test.go
+++ internal/transformer_test.go
@@ -17,24 +17,24 @@ func TestInputTransformer_Transform(t *testing.T) {
 			name:  "single coordinate",
 			input: "A0",
 			want: []Coordinate{
-				{x: 0, y: 0}, // A0 maps to (0,0)
+				{rowIndex: 0, columnIndex: 0}, // A0 maps to (0,0)
 			},
 		},
 		{
 			name:  "multiple coordinates same row",
 			input: "A0A1A2",
 			want: []Coordinate{
-				{x: 0, y: 0},
-				{x: 0, y: 1},
-				{x: 0, y: 2},
+				{rowIndex: 0, columnIndex: 0},
+				{rowIndex: 0, columnIndex: 1},
+				{rowIndex: 0, columnIndex: 2},
 			},
 		},
 		{
 			name:  "coordinates across rows",
 			input: "A35B0",
 			want: []Coordinate{
-				{x: 0, y: 35},
-				{x: 1, y: 0},
+				{rowIndex: 0, columnIndex: 35},
+				{rowIndex: 1, columnIndex: 0},
 			},
 		},
 		{
@@ -51,8 +51,8 @@ func TestInputTransformer_Transform(t *testing.T) {
 			name:  "last valid positions",
 			input: "F34F35",
 			want: []Coordinate{
-				{x: 5, y: 34},
-				{x: 5, y: 35},
+				{rowIndex: 5, columnIndex: 34},
+				{rowIndex: 5, columnIndex: 35},
 			},
 		},
 	}
